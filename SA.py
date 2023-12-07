4a8c0e0e9f3b10954698d9f4ce9b6d0ead7b0707
Syed-Shaheer-Ali-Zaidi <shaheerinkhi@gmail.com>
2023-12-08 02:26:36 +0500

    added semantic analysis for variable decleration

--- SA.py
+++ SA.py
@@ -3,6 +3,268 @@ f = open('D:\\Shaheer\\Compiler Construction\\Compiler-Construction\\code.txt', 
 code = f.read()
 TS = LA.breakWords(code)
 
+global ST
+global MT 
+global DT
+global scope
+global stack
+
+ST = []
+MT = []
+DT = []
+stack = []
+scope = 0
+
+def insertST (N,T,S, i):
+    if (len(T)>5):
+        if(lookupFuncST(N)==False):
+            ST.append([N, T, S])
+            return True
+        else:
+            print("ST Function redeclaration at line ", TS[i][2])
+            return False
+    else:
+        if(lookupST(N)==False):
+            ST.append([N, T, S])
+            return True
+        else:
+            print("ST Redeclaration at line ", TS[i][2])
+            return False
+    
+def insertDT (N, T, AM, CM, P, i):
+    if(lookupDT(N)==False):
+        DT.append([N, T, AM, CM, P])
+        return True
+    else:
+        print("DT Redeclaration at line ", TS[i][2])
+        return False
+    
+def insertMT (N, T, AM, TM, const, CN, i):
+    if(len(T)>5):
+        if(lookupFuncMT(N, CN)==False):
+            MT.append([N, T, AM, TM, const, CN])
+            return True
+        else:
+            print("MT Function Redeclaration at line ", TS[i][2])
+            return False
+    else:
+        if(lookupMT(N, CN)==False):
+            MT.append([N, T, AM, TM, const, CN])
+            return True
+        else:
+            print("MT Redeclaration at line ", TS[i][2])
+            return False
+
+def lookupST(N):
+    for i in range(len(ST)):
+        if(ST[i][0] == N):
+            t = i
+            temp = stack
+            counter = -1
+            for j in range (len(stack)):
+                if (ST[t][2]==temp[counter]):
+                    return ST[t][1]
+                counter -=1
+    return False
+def lookupDT(N):
+    for i in range(len(DT)):
+        if(DT[i][0] == N):
+            return DT[i]
+    return False
+
+def lookupMT(N, CN):
+    for i in range(len(MT)):
+        if((MT[i][0] == N and MT[i][5] == CN)):
+            return MT[i]
+    return False
+
+def lookupFuncST(N, PL):
+    for i in range(len(ST)):
+        if(ST[i][0] == N and ST[i][1] == PL):
+            t = i
+            temp = stack
+            ReTtype = []
+            Ret = []
+            counter = -1
+            for j in range (len(stack)):
+                if (ST[t][2]==temp[counter]):
+                    ReTtype = ST[t][1]
+                    for i in range (len(ReTtype)):
+                        if(ReTtype[i]==">"):
+                            remaining = len(ReTtype) - i - 1
+                            for j in remaining:
+                                Ret.append(ReTtype[i+j+1])
+                            return Ret
+            counter -= 1
+    return False
+
+def lookupFuncMT(N, PL, CN):
+    for i in range(len(MT)):
+        if((MT[i][0] == N and MT[i][5] == CN and MT[i][1]== PL)):
+            return MT[i]
+    return False
+
+def Compatibility(OP, LOT, ROT):
+    type_combinations = {
+
+    ('+', 'text', 'text'): 'text',    # "abc" + "def"
+    ('+', 'char', 'char'): 'text',    # 'a' + 'b'
+    ('+', 'int', 'int'): 'int',        # 1 + 1
+    ('+', 'flt', 'flt'): 'flt',        # 1.1 + 1.2
+    ('+', 'int', 'flt'): 'flt',        # 1 + 2.1
+    ('+', 'flt', 'int'): 'flt',        # 1.1 + 2
+    ('+', 'logic', 'logic'): 'int',     # True + True
+    ('+', 'logic', 'int'): 'int',       # True + 1
+    ('+', 'int', 'logic'): 'int',       # 1 + True
+    ('+', 'logic', 'flt'): 'flt',       # True + 1.1
+    ('+', 'flt', 'logic'): 'flt',       # 1.1 + True
+
+    ('-', 'int', 'int'): 'int',        # 1 - 1
+    ('-', 'flt', 'flt'): 'flt',        # 1.1 - 1.2
+    ('-', 'int', 'flt'): 'flt',        # 1 - 2.1
+    ('-', 'flt', 'int'): 'flt',        # 1.1 - 2
+    ('-', 'logic', 'logic'): 'int',     # True - True
+    ('-', 'logic', 'int'): 'int',       # True - 1
+    ('-', 'int', 'logic'): 'int',       # 1 - True
+    ('-', 'logic', 'flt'): 'flt',       # True - 1.1
+    ('-', 'flt', 'logic'): 'flt',       # 1.1 - True
+
+    ('*', 'int', 'int'): 'int',        # 1 * 1
+    ('*', 'flt', 'flt'): 'flt',        # 1.1 * 1.2
+    ('*', 'int', 'flt'): 'flt',        # 1 * 2.1
+    ('*', 'flt', 'int'): 'flt',        # 1.1 * 2
+    ('*', 'logic', 'logic'): 'int',   # True * True
+    ('*', 'logic', 'int'): 'int',       # True * 1
+    ('*', 'int', 'logic'): 'int',       # 1 * True
+    ('*', 'logic', 'flt'): 'flt',       # True * 1.1
+    ('*', 'flt', 'logic'): 'flt',       # 1.1 * True
+
+    ('/', 'int', 'int'): 'flt',        # 1 / 1
+    ('/', 'flt', 'flt'): 'flt',        # 1.1 / 1.2
+    ('/', 'int', 'flt'): 'flt',        # 1 / 2.1
+    ('/', 'flt', 'int'): 'flt',        # 1.1 / 2
+    ('/', 'logic', 'logic'): 'int',   # True / True
+    ('/', 'logic', 'int'): 'int',       # True / 1
+    ('/', 'int', 'logic'): 'int',       # 1 / True
+    ('/', 'logic', 'flt'): 'flt',       # True / 1.1
+    ('/', 'flt', 'logic'): 'flt',       # 1.1 / True
+
+    ('**', 'int', 'int'): 'int',        # 2^2
+    ('**', 'flt', 'flt'): 'flt',        # 2.2^1.1
+    ('**', 'int', 'flt'): 'flt',        # 2^1.1
+    ('**', 'flt', 'int'): 'flt',        # 1.1^2
+    ('**', 'logic', 'logic'): 'int',   # True^True
+    ('**', 'logic', 'int'): 'int',       # True^2
+    ('**', 'int', 'logic'): 'int',       # 2^True
+    ('**', 'logic', 'flt'): 'flt',       # True^1.1
+    ('**', 'flt', 'logic'): 'flt',       # 1.1^True
+
+    ('%', 'int', 'int'): 'flt',        # 1 % 1
+    ('%', 'flt', 'flt'): 'flt',        # 1.1 % 1.2
+    ('%', 'int', 'flt'): 'flt',        # 1 % 2.1
+    ('%', 'flt', 'int'): 'flt',        # 1.1 % 2
+    ('%', 'logic', 'logic'): 'flt',     # True % True
+    ('%', 'logic', 'int'): 'flt',       # True % 1
+    ('%', 'int', 'logic'): 'flt',       # 1 % True
+    ('%', 'logic', 'flt'): 'flt',       # True % 1.1
+    ('%', 'flt', 'logic'): 'flt',       # 1.1 % True
+
+    ('<', 'int', 'int'): 'logic',       # 1 < 1
+    ('<', 'flt', 'flt'): 'logic',       # 1.1 < 1.2
+    ('<', 'int', 'flt'): 'logic',       # 1 < 2.1
+    ('<', 'flt', 'int'): 'logic',       # 1.1 < 2
+    ('<', 'logic', 'logic'): 'logic',   # True / True
+    ('<', 'logic', 'int'): 'logic',       # True / 1
+    ('<', 'int', 'logic'): 'logic',       # 1 / True
+    ('<', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('<', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('>', 'int', 'int'): 'logic',       # 1 > 1
+    ('>', 'flt', 'flt'): 'logic',       # 1.1 > 1.2
+    ('>', 'int', 'flt'): 'logic',       # 1 > 2.1
+    ('>', 'flt', 'int'): 'logic',       # 1.1 > 2
+    ('>', 'logic', 'logic'): 'logic',   # True / True
+    ('>', 'logic', 'int'): 'logic',       # True / 1
+    ('>', 'int', 'logic'): 'logic',       # 1 / True
+    ('>', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('>', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('<=', 'int', 'int'): 'logic',       # 1 < 1
+    ('<=', 'flt', 'flt'): 'logic',       # 1.1 < 1.2
+    ('<=', 'int', 'flt'): 'logic',       # 1 < 2.1
+    ('<=', 'flt', 'int'): 'logic',       # 1.1 < 2
+    ('<=', 'logic', 'logic'): 'logic',   # True / True
+    ('<=', 'logic', 'int'): 'logic',       # True / 1
+    ('<=', 'int', 'logic'): 'logic',       # 1 / True
+    ('<=', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('<=', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('>=', 'int', 'int'): 'logic',       # 1 > 1
+    ('>=', 'flt', 'flt'): 'logic',       # 1.1 > 1.2
+    ('>=', 'int', 'flt'): 'logic',       # 1 > 2.1
+    ('>=', 'flt', 'int'): 'logic',       # 1.1 > 2
+    ('>=', 'logic', 'logic'): 'logic',   # True / True
+    ('>=', 'logic', 'int'): 'logic',       # True / 1
+    ('>=', 'int', 'logic'): 'logic',       # 1 / True
+    ('>=', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('>=', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('==', 'int', 'int'): 'logic',      # 1 == 1
+    ('==', 'flt', 'flt'): 'logic',      # 1.1 == 1.2
+    ('==', 'int', 'flt'): 'logic',      # 1 == 2.1
+    ('==', 'flt', 'int'): 'logic',      # 1.1 == 2
+    ('==', 'logic', 'logic'): 'logic',   # True / True
+    ('==', 'logic', 'int'): 'logic',       # True / 1
+    ('==', 'int', 'logic'): 'logic',       # 1 / True
+    ('==', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('==', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('!=', 'int', 'int'): 'logic',      # 1 != 1
+    ('!=', 'flt', 'flt'): 'logic',      # 1.1 != 1.2
+    ('!=', 'int', 'flt'): 'logic',      # 1 != 2.1
+    ('!=', 'flt', 'int'): 'logic',      # 1.1 != 2
+    ('!=', 'logic', 'logic'): 'logic',   # True / True
+    ('!=', 'logic', 'int'): 'logic',       # True / 1
+    ('!=', 'int', 'logic'): 'logic',       # 1 / True
+    ('!=', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('!=', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('and', 'logic', 'logic'): 'logic',       # True and True
+
+    ('or', 'logic', 'logic'): 'logic',       # True or True
+    
+    }
+
+    # Check if the combination is in the dictionary
+    if (OP, LOT, ROT) in type_combinations:
+        return type_combinations[(OP, LOT, ROT)]
+    else:
+        # Handle unsupported combinations
+        print ("Type Mismatch")
+        return False
+
+def Compatibility (OP, OT):
+
+    type_combinations = {
+          ('not', 'logic'): 'logic'
+    }
+
+    # Check if the combination is in the dictionary
+    if (OP, OT) in type_combinations:
+        return type_combinations[(OP, OT)]
+    else:
+        # Handle unsupported combinations
+        print ("Type Mismatch")
+        return False
+
+def CreateScope():
+    global scope
+    stack.append(scope)
+    scope+=1
+
+def DestroyScope():
+    stack.pop()
+
 def const(i):
     if (TS[i][0] == "INTCONST"):
         return i + 1, True
@@ -18,15 +280,17 @@ def const(i):
     return i, False 
     
 
-def list2(i):
+def list2(i, T):
     if (TS[i][0] == ";"):
         return i + 1, True
     if (TS[i][0] == "comma"):
         i+=1
         if (TS[i][0] == "ID"):
+            N = TS[i][1]
+            insertST(N, T, stack[-1], i)
             i+=1
             if (TS[i][0] == "comma" or TS[i][0] == ";" or TS[i][0] == "SAO"):
-                i, listLogic = list(i)
+                i, listLogic = list(i, T)
                 if (listLogic):
                     return i, True
                 else:
@@ -41,11 +305,14 @@ def list2(i):
         print("Error, expected a ; or , at ", TS[i][2], " in line number ", TS[i][2])
         return i, False
 
-def init(i):
+def init(i, T):
     if (TS[i][0] == "ID"):
+        N = TS[i][1]
+        if(lookupST(N)==False):
+            print("Undeclared variable ", N, " at line number ", TS[i][2])
         i+=1
         if (TS[i][0] == "comma" or TS[i][0] == ";" or TS[i][0] == "SAO"):
-            i, listLogic = list(i)
+            i, listLogic = list(i, T)
             if(listLogic):
                 return i, True
             else:
@@ -57,7 +324,7 @@ def init(i):
         i, const_logic = const(i)
         if (const_logic):
             if (TS[i][0] == ";" or TS[i][0] == "comma"):
-                i, list2Logic = list2(i)
+                i, list2Logic = list2(i, T)
                 if(list2Logic):
                     return i, True
                 else:
@@ -71,13 +338,15 @@ def init(i):
         return i, False
 
 
-def list(i):
+def list(i, T):
     if (TS[i][0] == "comma"):
         i+=1
         if (TS[i][0] == "ID"):
+            N = TS[i][1]
+            insertST(N, T, stack[-1], i)
             i+=1
             if (TS[i][0] == "comma" or TS[i][0] == ";" or TS[i][0] == "SAO"):
-                i, listLogic = list(i)
+                i, listLogic = list(i, T)
                 if(listLogic):
                     return i, True
                 else:
@@ -93,7 +362,7 @@ def list(i):
     elif (TS[i][0] == "SAO"):
         i+=1    
         if (TS[i][0] == "ID" or TS[i][0] == "INTCONST" or TS[i][0] == "FLTCONST" or TS[i][0] == "TEXTCONST" or TS[i][0] == "CHRCONST" or TS[i][0] == "LOGICCONST"):
-            i, initLogic = init(i)
+            i, initLogic = init(i, T)
             if(initLogic):
                 return i, True
             else:
@@ -107,11 +376,14 @@ def list(i):
 
 def Decl(i):
     if (TS[i][0] == "DT"):
+        T = TS[i][1]
         i+=1
         if (TS[i][0] == "ID"):
+            N = TS[i][1]
+            insertST(N, T, stack[-1], i)
             i+=1
             if (TS[i][0] == "comma" or TS[i][0] == ";" or TS[i][0] == "SAO"):
-                i, listLogic = list(i)
+                i, listLogic = list(i, T)
                 if(listLogic):
                     return i, True
                 else:
@@ -2191,6 +2463,8 @@ def S(i):
         else:
             return i, False
 
+CreateScope()
 i, logic = S(0)
+DestroyScope()
 if (logic):
     print ("Parsed Successfully !")
--- SmA.py
+++ SmA.py
@@ -1,43 +1,53 @@
 global ST
 global MT 
 global DT
+global scope
 
 ST = []
 MT = []
 DT = []
 stack = []
+scope = 0
 
 def insertST (N,T,S):
     if (len(T)>5):
+        if(lookupFuncST(N)==False):
+            ST.append([N, T, S])
+            return True
+        else:
+            print("ST Function redeclaration at line ", TS[i][2])
+            return False
+    else:
         if(lookupST(N)==False):
             ST.append([N, T, S])
             return True
         else:
             print("ST Redeclaration at line ", TS[i][2])
             return False
-    else:
-        if(lookupFuncST(N)==False):
-            ST.append([N, T, S])
-            return True
-        else:
-            print("ST Function redeclaration at line",TS[i][2])
-            return False
     
 def insertDT (N, T, AM, CM, P):
-    if(lookupDT(N)):
-        DT.append(N, T, AM, CM, P)
+    if(lookupDT(N)==False):
+        DT.append([N, T, AM, CM, P])
         return True
     else:
         print("DT Redeclaration at line ", TS[i][2])
         return False
     
 def insertMT (N, T, AM, TM, const, CN):
-    if(lookupMT(N, CN)):
-        MT.append(N, T, AM, TM, const, CN)
-        return True
+    if(len(T)>5):
+        if(lookupFuncMT(N, CN)==False):
+            MT.append([N, T, AM, TM, const, CN])
+            return True
+        else:
+            print("MT Function Redeclaration at line ", TS[i][2])
+            return False
     else:
-        print("MT Redeclaration at line ", TS[i][2])
-        return False
+        if(lookupMT(N, CN)==False):
+            MT.append([N, T, AM, TM, const, CN])
+            return True
+        else:
+            print("MT Redeclaration at line ", TS[i][2])
+            return False
 
 def lookupST(N):
     for i in range(len(ST)):
@@ -49,9 +59,17 @@ def lookupST(N):
                     return ST[t][1]
     return False
 def lookupDT(N):
-    return rowDT
+    for i in range(len(DT)):
+        if(DT[i][0] == N):
+            return DT[i]
+    return False
+
 def lookupMT(N, CN):
-    return rowMT
+    for i in range(len(MT)):
+        if((MT[i][0] == N and MT[i][5] == CN)):
+            return MT[i]
+    return False
+
 def lookupFuncST(N, PL):
     for i in range(len(ST)):
         if(ST[i][0] == N and ST[i][1] == PL):
@@ -71,8 +89,179 @@ def lookupFuncST(N, PL):
     return False
 
 def lookupFuncMT(N, PL, CN):
-    return rowDT
-def Compatibility (left operand type, right operand type, operator):
-    return type
-def Compatibility (operand type, operator):
-    return type
+    for i in range(len(MT)):
+        if((MT[i][0] == N and MT[i][5] == CN and MT[i][1]== PL)):
+            return MT[i]
+    return False
+
+# def Compatibility (LOT, ROT, OP):
+#     if OP in ['+', '-', '*', '/', '%']:
+#         if (OP == '+' and LOT is 'text' and ROT is 'text'):
+#             T = "text"
+#             return T
+#         elif (OP == '+' and LOT is 'char' and ROT is 'char'):
+#             T= "text"
+#             return T
+#         elif (OP == '-' and LOT is 'int' and ROT is 'int'):
+#             T= "int"
+#             return T
+
+def Compatibility(OP, LOT, ROT):
+    type_combinations = {
+
+    ('+', 'text', 'text'): 'text',    # "abc" + "def"
+    ('+', 'char', 'char'): 'text',    # 'a' + 'b'
+    ('+', 'int', 'int'): 'int',        # 1 + 1
+    ('+', 'flt', 'flt'): 'flt',        # 1.1 + 1.2
+    ('+', 'int', 'flt'): 'flt',        # 1 + 2.1
+    ('+', 'flt', 'int'): 'flt',        # 1.1 + 2
+    ('+', 'logic', 'logic'): 'int',     # True + True
+    ('+', 'logic', 'int'): 'int',       # True + 1
+    ('+', 'int', 'logic'): 'int',       # 1 + True
+    ('+', 'logic', 'flt'): 'flt',       # True + 1.1
+    ('+', 'flt', 'logic'): 'flt',       # 1.1 + True
+
+    ('-', 'int', 'int'): 'int',        # 1 - 1
+    ('-', 'flt', 'flt'): 'flt',        # 1.1 - 1.2
+    ('-', 'int', 'flt'): 'flt',        # 1 - 2.1
+    ('-', 'flt', 'int'): 'flt',        # 1.1 - 2
+    ('-', 'logic', 'logic'): 'int',     # True - True
+    ('-', 'logic', 'int'): 'int',       # True - 1
+    ('-', 'int', 'logic'): 'int',       # 1 - True
+    ('-', 'logic', 'flt'): 'flt',       # True - 1.1
+    ('-', 'flt', 'logic'): 'flt',       # 1.1 - True
+
+    ('*', 'int', 'int'): 'int',        # 1 * 1
+    ('*', 'flt', 'flt'): 'flt',        # 1.1 * 1.2
+    ('*', 'int', 'flt'): 'flt',        # 1 * 2.1
+    ('*', 'flt', 'int'): 'flt',        # 1.1 * 2
+    ('*', 'logic', 'logic'): 'int',   # True * True
+    ('*', 'logic', 'int'): 'int',       # True * 1
+    ('*', 'int', 'logic'): 'int',       # 1 * True
+    ('*', 'logic', 'flt'): 'flt',       # True * 1.1
+    ('*', 'flt', 'logic'): 'flt',       # 1.1 * True
+
+    ('/', 'int', 'int'): 'flt',        # 1 / 1
+    ('/', 'flt', 'flt'): 'flt',        # 1.1 / 1.2
+    ('/', 'int', 'flt'): 'flt',        # 1 / 2.1
+    ('/', 'flt', 'int'): 'flt',        # 1.1 / 2
+    ('/', 'logic', 'logic'): 'int',   # True / True
+    ('/', 'logic', 'int'): 'int',       # True / 1
+    ('/', 'int', 'logic'): 'int',       # 1 / True
+    ('/', 'logic', 'flt'): 'flt',       # True / 1.1
+    ('/', 'flt', 'logic'): 'flt',       # 1.1 / True
+
+    ('**', 'int', 'int'): 'int',        # 2^2
+    ('**', 'flt', 'flt'): 'flt',        # 2.2^1.1
+    ('**', 'int', 'flt'): 'flt',        # 2^1.1
+    ('**', 'flt', 'int'): 'flt',        # 1.1^2
+    ('**', 'logic', 'logic'): 'int',   # True^True
+    ('**', 'logic', 'int'): 'int',       # True^2
+    ('**', 'int', 'logic'): 'int',       # 2^True
+    ('**', 'logic', 'flt'): 'flt',       # True^1.1
+    ('**', 'flt', 'logic'): 'flt',       # 1.1^True
+
+    ('%', 'int', 'int'): 'flt',        # 1 % 1
+    ('%', 'flt', 'flt'): 'flt',        # 1.1 % 1.2
+    ('%', 'int', 'flt'): 'flt',        # 1 % 2.1
+    ('%', 'flt', 'int'): 'flt',        # 1.1 % 2
+    ('%', 'logic', 'logic'): 'flt',     # True % True
+    ('%', 'logic', 'int'): 'flt',       # True % 1
+    ('%', 'int', 'logic'): 'flt',       # 1 % True
+    ('%', 'logic', 'flt'): 'flt',       # True % 1.1
+    ('%', 'flt', 'logic'): 'flt',       # 1.1 % True
+
+    ('<', 'int', 'int'): 'logic',       # 1 < 1
+    ('<', 'flt', 'flt'): 'logic',       # 1.1 < 1.2
+    ('<', 'int', 'flt'): 'logic',       # 1 < 2.1
+    ('<', 'flt', 'int'): 'logic',       # 1.1 < 2
+    ('<', 'logic', 'logic'): 'logic',   # True / True
+    ('<', 'logic', 'int'): 'logic',       # True / 1
+    ('<', 'int', 'logic'): 'logic',       # 1 / True
+    ('<', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('<', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('>', 'int', 'int'): 'logic',       # 1 > 1
+    ('>', 'flt', 'flt'): 'logic',       # 1.1 > 1.2
+    ('>', 'int', 'flt'): 'logic',       # 1 > 2.1
+    ('>', 'flt', 'int'): 'logic',       # 1.1 > 2
+    ('>', 'logic', 'logic'): 'logic',   # True / True
+    ('>', 'logic', 'int'): 'logic',       # True / 1
+    ('>', 'int', 'logic'): 'logic',       # 1 / True
+    ('>', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('>', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('<=', 'int', 'int'): 'logic',       # 1 < 1
+    ('<=', 'flt', 'flt'): 'logic',       # 1.1 < 1.2
+    ('<=', 'int', 'flt'): 'logic',       # 1 < 2.1
+    ('<=', 'flt', 'int'): 'logic',       # 1.1 < 2
+    ('<=', 'logic', 'logic'): 'logic',   # True / True
+    ('<=', 'logic', 'int'): 'logic',       # True / 1
+    ('<=', 'int', 'logic'): 'logic',       # 1 / True
+    ('<=', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('<=', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('>=', 'int', 'int'): 'logic',       # 1 > 1
+    ('>=', 'flt', 'flt'): 'logic',       # 1.1 > 1.2
+    ('>=', 'int', 'flt'): 'logic',       # 1 > 2.1
+    ('>=', 'flt', 'int'): 'logic',       # 1.1 > 2
+    ('>=', 'logic', 'logic'): 'logic',   # True / True
+    ('>=', 'logic', 'int'): 'logic',       # True / 1
+    ('>=', 'int', 'logic'): 'logic',       # 1 / True
+    ('>=', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('>=', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('==', 'int', 'int'): 'logic',      # 1 == 1
+    ('==', 'flt', 'flt'): 'logic',      # 1.1 == 1.2
+    ('==', 'int', 'flt'): 'logic',      # 1 == 2.1
+    ('==', 'flt', 'int'): 'logic',      # 1.1 == 2
+    ('==', 'logic', 'logic'): 'logic',   # True / True
+    ('==', 'logic', 'int'): 'logic',       # True / 1
+    ('==', 'int', 'logic'): 'logic',       # 1 / True
+    ('==', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('==', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('!=', 'int', 'int'): 'logic',      # 1 != 1
+    ('!=', 'flt', 'flt'): 'logic',      # 1.1 != 1.2
+    ('!=', 'int', 'flt'): 'logic',      # 1 != 2.1
+    ('!=', 'flt', 'int'): 'logic',      # 1.1 != 2
+    ('!=', 'logic', 'logic'): 'logic',   # True / True
+    ('!=', 'logic', 'int'): 'logic',       # True / 1
+    ('!=', 'int', 'logic'): 'logic',       # 1 / True
+    ('!=', 'logic', 'flt'): 'logic',       # True / 1.1
+    ('!=', 'flt', 'logic'): 'logic',       # 1.1 / True
+
+    ('and', 'logic', 'logic'): 'logic',       # True and True
+
+    ('or', 'logic', 'logic'): 'logic',       # True or True
+    
+    }
+
+    # Check if the combination is in the dictionary
+    if (OP, LOT, ROT) in type_combinations:
+        return type_combinations[(OP, LOT, ROT)]
+    else:
+        # Handle unsupported combinations
+        print ("Type Mismatch")
+        return False
+
+def Compatibility (OP, OT):
+
+    type_combinations = {
+          ('not', 'logic'): 'logic'
+    }
+
+    # Check if the combination is in the dictionary
+    if (OP, OT) in type_combinations:
+        return type_combinations[(OP, OT)]
+    else:
+        # Handle unsupported combinations
+        print ("Type Mismatch")
+        return False
+
+def CreateScope():
+    stack.append(scope)
+    scope+=1
+
+def DestroyScope():
+    stack.pop()
